870dcb9245d2fbc7e08eacdfddb8805f9bf4e5ca
helena <helena.gilja@gmail.com>
2022-11-09 21:31:33 +0000

    JS file added to the project

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+let topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+for (let i = 0; i < topics.length; i++) {
+  console.log(topics[i]);
+}
+
+// if (topics === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (topics === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (topics === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (topics === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// }
